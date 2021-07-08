0433d2e715d119226686adc264429f82512467b8
engmarcoswilson <engmarcoswilson@gmail.com>
2021-07-08 04:46:58 -0300

    Adicionando arq csv TLCD não linear

---
+++ TLCD_n_linear_forca_bruta_varredura.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+"""
+TLCD não-linear
+Varredura harmônica por força Bruta
+Plotagem gráficos t x w0 para 10 frequências
+Marcos Wilson
+"""
+
+import math
+import numpy as np
+from scipy.integrate import odeint
+from matplotlib import pyplot as plt
+from TLCD_functions import tlcd_estrutura
+from TLCD_functions import tlcd_n_linear
+
+#Condição inicial
+n = 500
+t = np.linspace(0, 400, n)
+winit = (0, 0)
+
+#Excitação
+Omg_exc = np.logspace(-1, 1, 500)
+u0 = 1
+
+#Dados do TLCD
+u0 =1
+e_L=0
+b = 2
+H = 1
+L = 2*H+b
+alfa = b/L
+g = 9.81
+wa = np.sqrt((2*g/L))
+
+#Varredura: Força Bruta
+F_exc = np.array([])
+
+for i in range(0, 500, 50):
+  F_exc = np.append(F_exc, Omg_exc[i])
+
+w0_n = np.zeros(len(F_exc))
+  
+for i in range(0, len(F_exc)):
+  print('Frequência analisada:  %.3f Hz'%F_exc[i])
+  warr = odeint(tlcd_estrutura, winit, t, args=(e_L, wa, alfa, u0, F_exc[i]))
+  w0_n[i] = tlcd_n_linear(tlcd_estrutura, winit, t, e_L, wa, alfa, u0, F_exc[i])
+  w = w0_n[i]*np.cos(F_exc[i]*2*np.pi*t)
+  
+  plt.figure(figsize=(16, 10))
+  plt.plot(t, warr[:, 0], label='w(t)')
+  plt.plot(t[int(len(t)/2):int(len(t))], w[int(len(t)/2):int(len(t))], label='w(t)')
+  plt.rcParams['legend.fontsize'] = 12
+  plt.legend(loc='upper right', prop={'size':16})
+  plt.xlabel("t (s)")
+  plt.ylabel('Amplitude')
+  plt.legend(loc='best')
+  plt.savefig('varredura_forca_bruta%.3f.png'%F_exc[i], format='png')
+  plt.show()
--- TLCD_n_linear_simulation2.py
+++ TLCD_n_linear_simulation2.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 """
-Created on Thu Jul  8 01:24:20 2021
-
-@author: User
+Simulação TLCD não-linear
+Gerando arquivos csv (Omg_exc x w0)
+Marcos Wilson
 """
 
 import math
@@ -14,12 +14,12 @@ from TLCD_functions import tlcd_n_linear
 import csv
 
 #Condição inicial
-n = 5000
+n = 500
 t = np.linspace(0, 400, n)
 winit = (0, 0)
 
 #Excitação
-Omg_exc = np.logspace(0, 2.5, 500)
+Omg_exc = np.logspace(-1, 1, 500)
 u0 = 1
 
 #Dados do TLCD
@@ -35,25 +35,10 @@ wa = np.sqrt((2*g/L))
 w0 = np.zeros(len(Omg_exc))
 
 #Arquivo csv: Omg_exc x w0
-f = open('TLCD_freq_x_w0', 'w', newline='', encoding = 'utf-8')
+f = open('TLCD_freq_x_w0.csv', 'w', newline='', encoding = 'utf-8')
 w = csv.writer(f)
 
 #Simulação
 for i in range(0, 500):
   w0[i] = tlcd_n_linear(tlcd_estrutura, winit, t, e_L, wa, alfa, u0, Omg_exc[i])
   w.writerow([Omg_exc[i], w0[i]])
-
- 
-plt.figure(figsize=(12,8))
-plt.plot(Omg_exc, abs(w0), "r--",label='u - numérico')
-plt.xscale("log")
-plt.yscale("log")
-plt.rc('axes', titlesize=16)     # fontsize of the axes title
-plt.rc('axes', labelsize=16)    # fontsize of the x and y labels
-plt.rcParams.update({'font.size': 16})
-plt.xlabel("$\Omega_{exc} [Hz]$")
-plt.ylabel('u')
-plt.legend(loc='best', fontsize=10)
-plt.grid()
-plt.savefig('TLCD_freq_x_w0', format='png')
-plt.show()
